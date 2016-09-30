ae8e75c35a6c6f37a7fa02d6aea785240c037a86
DiegoAMedina <DiegoAMedina@users.noreply.github.com>
2016-09-30 11:24:55 -0700

    Filtered the array given
    Iterated through the array given,  and created a new array without the words that began with the prefix provided.

--- prefix.py
+++ prefix.py
@@ -23,4 +23,26 @@ dictionaryRequest = requests.post('http://challenge.code2040.org/api/prefix', lo
 # converts dictionary into json dictionary
 dictionaryRequest = dictionaryRequest.json()
 
-print dictionaryRequest
+#-------------------------------------- create new array with out the words including the prefix
+
+# new variable to make the code easier to read.
+prefix = dictionaryRequest['prefix']
+
+# newArray variable will be the string of the new array without the words beginning with the prefix given.
+newList = []
+
+iterator = 0
+
+# iterates through array of strings
+for key in dictionaryRequest['array']:
+	# checks to see if a word begins with the prefix provided
+	if (key.startswith( prefix, 0, len(prefix) ) ):
+		continue
+	# if it does not start with it then add it to the newArray
+	else:
+		newList.append( str(key) )
+
+newDictionary = {'token':'e2f2c742e71d0ea6a9f5e02545228de9', 'array': newList}
+ 
+
+
